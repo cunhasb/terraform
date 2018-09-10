8103784a7ebcb2c6a9354f04f565a1fd5d80dabf
Fabiano Cunha <fabianocunhadev@gmail.com>
2018-09-09 21:08:28 -0400

    start 4 instances

--- example.tf
+++ example.tf
@@ -2,48 +2,25 @@
 #$export AWS_SECRET_ACCESS_KEY="asecretkey"
 #$export AWS_DEFAULT_REGION="us-east-1"
 
+module "ubuntu-1" {
+  source ="./modules/"
+  instance_name="Ubuntu-1"
+}
 
 
-module "elb_http" {
-  source = "terraform-aws-modules/elb/aws"
+module "ubuntu-2" {
+  source ="./modules/"
+  instance_name="Ubuntu-2"
+}
 
-  name = "elb-example"
 
-  subnets         = ["subnet-12345678", "subnet-87654321"]
-  security_groups = ["sg-12345678"]
-  internal        = false
+module "ubuntu-3" {
+  source ="./modules/"
+  instance_name="Ubuntu-3"
+}
 
-  listener = [
-    {
-      instance_port     = "80"
-      instance_protocol = "HTTP"
-      lb_port           = "80"
-      lb_protocol       = "HTTP"
-    },
-  ]
 
-  health_check = [
-    {
-      target              = "HTTP:80/"
-      interval            = 30
-      healthy_threshold   = 2
-      unhealthy_threshold = 2
-      timeout             = 5
-    },
-  ]
-
-  access_logs = [
-    {
-      bucket = "my-access-logs-bucket"
-    },
-  ]
-
-  // ELB attachments
-  number_of_instances = 2
-  instances           = ["i-06ff41a77dfb5349d", "i-4906ff41a77dfb53d"]
-
-  tags = {
-    Owner       = "user"
-    Environment = "dev"
-  }
+module "ubuntu-4" {
+  source ="./modules/"
+  instance_name="Ubuntu-4"
 }
